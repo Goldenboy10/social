71a6782cc32433ff478393a746826a4338061c52
Mickaele Perez <mick.p311@gmail.com>
2017-06-08 15:26:00 -0500

    Sign in UI custom classes complete

---
+++ social/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  social
+//
+//  Created by Mickaele Perez on 6/8/17.
+//  Copyright © 2017 Code. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+
---
+++ social/FancyView.swift
@@ -0,0 +1,26 @@
+//
+//  FancyView.swift
+//  social
+//
+//  Created by Mickaele Perez on 6/8/17.
+//  Copyright © 2017 Code. All rights reserved.
+//
+
+import UIKit
+
+class FancyView: UIView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        //color of the shadow
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        
+        
+        
+    }
+
+}
